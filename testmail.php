f9c92fb6a7a2a0dadc1a3729613e360bf9adfec4
tineo <cesar@tineo.mobi>
2018-01-26 15:07:35 -0500

    add mailgun smtp

--- testmail.php
+++ testmail.php
@@ -5,5 +5,20 @@
  * Date: 26/01/18
  * Time: 03:00 PM
  */
-include_once 'ajax/meowler.php';
-meow("itsudatte01@gmail.com","Test de mailgun", "Hola desde Heroku","");
+require_once 'vendor/autoload.php';
+
+$transport = (new Swift_SmtpTransport(getenv("MAILGUN_SMTP_SERVER"), getenv("MAILGUN_SMTP_PORT"), 'ssl'))
+	->setUsername(getenv("MAILGUN_SMTP_LOGIN"))
+	->setPassword(getenv("MAILGUN_SMTP_PASSWORD"));
+
+// Create the Mailer using your created Transport
+$mailer = new Swift_Mailer($transport);
+
+// Create a message
+$message = (new Swift_Message("Test"))
+	->setFrom(['cesar@tineo.mobi' => 'Cesar Gutierrez Tineo ;D'])
+	->setTo(["itsudatte01@gmail.com"])
+	->setBody("Desde heroku", 'text/html');
+
+// Send the message
+echo $mailer->send($message);
